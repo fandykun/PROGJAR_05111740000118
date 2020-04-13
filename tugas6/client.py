4aee301802c327d2ac34c0da0642179f5ea9c995
fandykun <fandy19061999@gmail.com>
2020-04-13 21:27:53 +0700

    upload tugas 6

---
+++ tugas6/client.py
@@ -0,0 +1,24 @@
+import json
+import socket
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_address = ('127.0.0.1', 10001)
+
+print(f"connecting to {server_address}")
+sock.connect(server_address)
+
+try:
+    data = 'GET / HTTP/1.0'+"\r\n\r\n"
+    sock.send(data.encode())
+    receivemsg = ""
+    while True:
+       dataa = sock.recv(32)
+       if (dataa):
+           receivemsg = receivemsg + dataa.decode()  #data harus didecode agar dapat di operasikan dalam bentuk string
+           if receivemsg[-4:]=='\r\n\r\n':
+               print(receivemsg)
+               break
+
+finally:
+    print("closing socket")
+    sock.close()
